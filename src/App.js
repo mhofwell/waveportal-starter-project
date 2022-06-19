0d5eccce54ec7f3925924f234332569a9d36e72b
Michael Hofweller <michael.hofweller@gmail.com>
2022-06-19 16:29:10 -0700

    wave counter public facing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
 
   const contractAddress = '0x3b2C6C417651E48cdB66F8520278C2B2569d56b7';
   const contractABI = abi.abi;
+  let totalWaves; 
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -77,6 +78,7 @@ const wave = async () => {
      console.log("Mined ---", waveTxn.hash);
 
      count = await wavePortalContract.getTotalWaves(); 
+     totalWaves = count; 
      console.log("Retrieved total wave count...", count.toNumber());
 
     } else {
@@ -99,7 +101,10 @@ return (
       </div>
 
       <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Michael and I worked at Microsoft so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      </div>
+      <div className="bio">
+        This is how many people waved at me! {totalWaves}
       </div>
 
       <button className="waveButton" onClick={wave}>
