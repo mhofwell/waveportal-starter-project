5437fe2b0a621192493d045c337b82db275e2a23
Michael Hofweller <michael.hofweller@gmail.com>
2022-06-21 16:48:57 -0700

    put checkwallet into useeffect

--- src/App.js
+++ src/App.js
@@ -49,33 +49,6 @@ const App = () => {
       console.log(error);
     }
   }
-
-
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const {ethereum} = window; 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return; 
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
-
-      const accounts = await ethereum.request({method: "eth_accounts"});
-
-      if (accounts.length !== 0) {
-        const account = accounts[0]; 
-        console.log("Found an authorized account:", account); 
-        setCurrentAccount(account);
-        getAllWaves();
-
-      } else {
-        console.log("no authorized account found")
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
   /**
   * Implement your connectWallet method here
   */
@@ -129,10 +102,31 @@ const wave = async () => {
   }
 }
 
-
 useEffect(() => {
-  checkIfWalletIsConnected();
-},)
+  try {
+    const {ethereum} = window; 
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return; 
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({method: "eth_accounts"});
+
+    if (accounts.length !== 0) {
+      getAllWaves(); 
+      const account = accounts[0]; 
+      console.log("Found an authorized account:", account); 
+      setCurrentAccount(account);
+      
+    } else {
+      console.log("no authorized account found")
+    }
+  } catch (error) {
+    console.log(error);
+  }
+},[])
 
 return (
   <div className="mainContainer">
